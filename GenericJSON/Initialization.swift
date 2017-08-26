ceac56abeddc152e303d88e22fb0bb4e6f161280
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2017-08-26 10:16:58 +0200

    Add some basic documentation comments

--- GenericJSON/Initialization.swift
+++ GenericJSON/Initialization.swift
@@ -1,5 +1,41 @@
 import Foundation
 
+extension JSON {
+
+    /// Create a JSON value from anything. Argument has to be a valid JSON structure:
+    /// A `Float`, `Int`, `String`, `Bool`, an `Array` of those types or a `Dictionary`
+    /// of those types.
+    public init(_ value: Any) throws {
+        switch value {
+            case let num as Float:
+                self = .number(num)
+            case let num as Int:
+                self = .number(Float(num))
+            case let str as String:
+                self = .string(str)
+            case let bool as Bool:
+                self = .bool(bool)
+            case let array as [Any]:
+                self = .array(try array.map(JSON.init))
+            case let dict as [String:Any]:
+                self = .object(try dict.mapValues(JSON.init))
+            default:
+                throw Error.decodingError
+        }
+    }
+}
+
+extension JSON {
+
+    /// Create a JSON value from a `Codable`. This will give you access to the “raw”
+    /// encoded JSON value the `Codable` is serialized into. And hopefully, you could
+    /// encode the resulting JSON value and decode the original `Codable` back.
+    public init<T: Codable>(codable: T) throws {
+        let encoded = try JSONEncoder().encode(codable)
+        self = try JSONDecoder().decode(JSON.self, from: encoded)
+    }
+}
+
 extension JSON: ExpressibleByBooleanLiteral {
 
     public init(booleanLiteral value: Bool) {
@@ -50,35 +86,5 @@ extension JSON: ExpressibleByStringLiteral {
 
     public init(stringLiteral value: String) {
         self = .string(value)
-    }
-}
-
-extension JSON {
-
-    public init(_ value: Any) throws {
-        switch value {
-            case let num as Float:
-                self = .number(num)
-            case let num as Int:
-                self = .number(Float(num))
-            case let str as String:
-                self = .string(str)
-            case let bool as Bool:
-                self = .bool(bool)
-            case let array as [Any]:
-                self = .array(try array.map(JSON.init))
-            case let dict as [String:Any]:
-                self = .object(try dict.mapValues(JSON.init))
-            default:
-                throw Error.decodingError
-        }
-    }
-}
-
-extension JSON {
-
-    public init<T: Codable>(codable: T) throws {
-        let encoded = try JSONEncoder().encode(codable)
-        self = try JSONDecoder().decode(JSON.self, from: encoded)
     }
 }
