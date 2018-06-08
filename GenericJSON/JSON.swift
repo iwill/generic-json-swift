bbd587481a7ce1ee04e24ba498d5d785a5116e04
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2018-06-08 17:31:31 +0200

    Drop hand-written Equatable conformance

--- GenericJSON/JSON.swift
+++ GenericJSON/JSON.swift
@@ -4,35 +4,13 @@ import Foundation
 /// for JSON values, since it makes sure only valid JSON values are present & supports `Equatable`
 /// and `Codable`, so that you can compare values for equality and code and decode them into data
 /// or strings.
-public enum JSON {
+public enum JSON: Equatable {
     case string(String)
     case number(Float)
     case object([String:JSON])
     case array([JSON])
     case bool(Bool)
     case null
-}
-
-extension JSON: Equatable {
-
-    public static func == (lhs: JSON, rhs: JSON) -> Bool {
-        switch (lhs, rhs) {
-        case (.string(let s1), .string(let s2)):
-            return s1 == s2
-        case (.number(let n1), .number(let n2)):
-            return n1 == n2
-        case (.object(let o1), .object(let o2)):
-            return o1 == o2
-        case (.array(let a1), .array(let a2)):
-            return a1 == a2
-        case (.bool(let b1), .bool(let b2)):
-            return b1 == b2
-        case (.null, .null):
-            return true
-        default:
-            return false
-        }
-    }
 }
 
 extension JSON: CustomDebugStringConvertible {
