9549ff269635a2f755acbae5a6d871fa7c1b84fa
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2018-06-08 17:37:01 +0200

    Reduce code fragmentation

--- GenericJSON/JSON.swift
+++ GenericJSON/JSON.swift
@@ -32,3 +32,49 @@ extension JSON: CustomDebugStringConvertible {
         }
     }
 }
+
+extension JSON: Codable {
+
+    public func encode(to encoder: Encoder) throws {
+
+        var container = encoder.singleValueContainer()
+
+        switch self {
+        case let .array(array):
+            try container.encode(array)
+        case let .object(object):
+            try container.encode(object)
+        case let .string(string):
+            try container.encode(string)
+        case let .number(number):
+            try container.encode(number)
+        case let .bool(bool):
+            try container.encode(bool)
+        case .null:
+            try container.encodeNil()
+        }
+    }
+
+    public init(from decoder: Decoder) throws {
+
+        let container = try decoder.singleValueContainer()
+
+        if let object = try? container.decode([String: JSON].self) {
+            self = .object(object)
+        } else if let array = try? container.decode([JSON].self) {
+            self = .array(array)
+        } else if let string = try? container.decode(String.self) {
+            self = .string(string)
+        } else if let bool = try? container.decode(Bool.self) {
+            self = .bool(bool)
+        } else if let number = try? container.decode(Float.self) {
+            self = .number(number)
+        } else if container.decodeNil() {
+            self = .null
+        } else {
+            throw DecodingError.dataCorrupted(
+                .init(codingPath: decoder.codingPath, debugDescription: "Invalid JSON value.")
+            )
+        }
+    }
+}
