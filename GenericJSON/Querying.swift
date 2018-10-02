04dafd5b496240f974ee53858a779bda9d0b3784
Cameron McOnie <cjmconie@gmail.com>
2018-10-02 17:04:44 +0200

    add accessor and subscript setters

--- GenericJSON/Querying.swift
+++ GenericJSON/Querying.swift
@@ -1,47 +1,92 @@
 import Foundation
 
 public extension JSON {
-
+    
     /// Return the string value if this is a `.string`, otherwise `nil`
     public var stringValue: String? {
-        if case .string(let value) = self {
-            return value
+        get {
+            if case .string(let value) = self {
+                return value
+            }
+            return nil
         }
-        return nil
+        set {
+            if let newValue = newValue {
+                self = JSON(stringLiteral: newValue)
+            } else {
+                self = JSON.null
+            }
+        }
     }
-
+    
     /// Return the float value if this is a `.number`, otherwise `nil`
     public var floatValue: Float? {
-        if case .number(let value) = self {
-            return value
+        get {
+            if case .number(let value) = self {
+                return value
+            }
+            return nil
         }
-        return nil
+        set {
+            if let newValue = newValue {
+                self = JSON(floatLiteral: newValue)
+            } else {
+                self = JSON.null
+            }
+        }
     }
-
+    
     /// Return the bool value if this is a `.bool`, otherwise `nil`
     public var boolValue: Bool? {
-        if case .bool(let value) = self {
-            return value
+        get {
+            if case .bool(let value) = self {
+                return value
+            }
+            return nil
         }
-        return nil
+        set {
+            if let newValue = newValue {
+                self = JSON(booleanLiteral: newValue)
+            } else {
+                self = JSON.null
+            }
+        }
     }
-
+    
     /// Return the object value if this is an `.object`, otherwise `nil`
     public var objectValue: [String: JSON]? {
-        if case .object(let value) = self {
-            return value
+        get {
+            if case .object(let value) = self {
+                return value
+            }
+            return nil
         }
-        return nil
+        set {
+            if let newValue = newValue {
+                self = JSON.object(newValue)
+            } else {
+                self = JSON.null
+            }
+        }
     }
-
+    
     /// Return the array value if this is an `.array`, otherwise `nil`
     public var arrayValue: [JSON]? {
-        if case .array(let value) = self {
-            return value
+        get {
+            if case .array(let value) = self {
+                return value
+            }
+            return nil
         }
-        return nil
+        set {
+            if let newValue = newValue {
+                self =  JSON.array(newValue)
+            } else {
+                self = JSON.null
+            }
+        }
     }
-
+    
     /// Return `true` iff this is `.null`
     public var isNull: Bool {
         if case .null = self {
@@ -49,22 +94,51 @@ public extension JSON {
         }
         return false
     }
-
+    
     /// If this is an `.array`, return item at index
     ///
     /// If this is not an `.array` or the index is out of bounds, returns `nil`.
     public subscript(index: Int) -> JSON? {
-        if case .array(let arr) = self, arr.indices.contains(index) {
-            return arr[index]
+        get {
+            if case .array(let arr) = self, arr.indices.contains(index) {
+                return arr[index]
+            }
+            return nil
         }
-        return nil
+        set {
+            if case .array(var arr) = self, arr.indices.contains(index) {
+                if let newValue = newValue {
+                    arr[index] = newValue // update value at index
+                    
+                    /*
+                     SwiftyJSON uses a backing type and updates the value at the index rather than copying, updating,
+                     and replacing the array.
+                     https://github.com/SwiftyJSON/SwiftyJSON/blob/3f6f1d32df6e5f334593682b20bb829e48bffbee/Source/SwiftyJSON.swift#L413
+                     
+                     My method may be inefficient?
+                     */
+                    
+                    self = JSON.array(arr)
+                } else {
+                    self = JSON.null
+                }
+            }
+        }
     }
-
+    
     /// If this is an `.object`, return item at key
     public subscript(key: String) -> JSON? {
-        if case .object(let dict) = self {
-            return dict[key]
+        get {
+            if case .object(let dict) = self {
+                return dict[key]
+            }
+            return nil
         }
-        return nil
+        set {
+            if case .object(var obj) = self {
+                obj[key] = newValue
+                self = JSON.object(obj)
+            }
+        }
     }
 }
