a61caf10858500adc60921f476028f49e143606e
Cameron McOnie <cjmconie@gmail.com>
2018-11-17 10:35:47 +0200

    raise access level to public

--- GenericJSON/KeyPath.swift
+++ GenericJSON/KeyPath.swift
@@ -1,19 +1,19 @@
-// Credit to Ole Begemann - https://oleb.net/blog/2017/01/dictionary-key-paths/
+// Credit: Ole Begemann - https://oleb.net/blog/2017/01/dictionary-key-paths/
 
 import Foundation
 
 /// Struct that models a key path as an array of segments.
-struct KeyPath {
-    private(set) var segments: [String]
+public struct KeyPath {
+    public private(set) var segments: [String]
     
-    var isEmpty: Bool { return segments.isEmpty }
-    var path: String {
+    public var isEmpty: Bool { return segments.isEmpty }
+    public var path: String {
         return segments.joined(separator: ".")
     }
     
     /// Strips off the first segment and returns a pair consisting of the first segment and the remaining key path.
     /// Returns `nil` if the key path has no segments.
-    func headAndTail() -> (head: String, tail: KeyPath)? {
+    public func headAndTail() -> (head: String, tail: KeyPath)? {
         guard !isEmpty else { return nil }
         var tail = segments
         let head = tail.removeFirst()
@@ -24,19 +24,19 @@ struct KeyPath {
 
 /// Initializes a KeyPath with a string of the form "this.is.a.keypath"
 extension KeyPath {
-    init(_ string: String) {
+    public init(_ string: String) {
         segments = string.components(separatedBy: ".")
     }
 }
 
 extension KeyPath: ExpressibleByStringLiteral {
-    init(stringLiteral value: String) {
+    public init(stringLiteral value: String) {
         self.init(value)
     }
-    init(unicodeScalarLiteral value: String) {
+    public init(unicodeScalarLiteral value: String) {
         self.init(value)
     }
-    init(extendedGraphemeClusterLiteral value: String) {
+    public init(extendedGraphemeClusterLiteral value: String) {
         self.init(value)
     }
 }
