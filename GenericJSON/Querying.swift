f30009ba73e82fc01a4b31adf2f023a96566706c
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2019-05-11 14:06:46 +0200

    Add array and key subscript writing

--- GenericJSON/Querying.swift
+++ GenericJSON/Querying.swift
@@ -50,29 +50,67 @@ public extension JSON {
         return false
     }
 
-    /// If this is an `.array`, return item at index
+    /// Get or set item on given index in an array
     ///
-    /// If this is not an `.array` or the index is out of bounds, returns `nil`.
+    /// For out-of-bounds indexes, reading returns `nil` and writing does nothing.
+    /// For non-array values, reading returns `nil` and writing converts the value
+    /// into an empty array. When writing, `nil` values get converted to `JSON.null`.
     subscript(index: Int) -> JSON? {
-        if case .array(let arr) = self, arr.indices.contains(index) {
+
+        get {
+            // Reading from a non-array value returns `nil`
+            guard case .array(let arr) = self else { return nil }
+            // Reading from an out-of-bounds index returns `nil`
+            guard arr.indices.contains(index) else { return nil }
+            // Regular access
             return arr[index]
         }
-        return nil
+
+        set {
+            // Writing to a non-array value converts the value into an empty array.
+            // TODO: This is not really very helpful, can we do better?
+            guard case .array(var arr) = self else {
+                self = .array([])
+                return
+            }
+
+            // Writing to an out-of-bounds index does nothing
+            guard arr.indices.contains(index) else { return }
+
+            // Regular access
+            arr[index] = newValue ?? .null
+            self = .array(arr)
+        }
     }
 
-    /// If this is an `.object`, return item at key
+    /// Get or set item under given key in an object
+    ///
+    /// For non-object values, reading returns `nil` and writing converts the value
+    /// into an object containing just the key & value being set.
     subscript(key: String) -> JSON? {
-        if case .object(let dict) = self {
+        get {
+            // Reading from a non-object value returns `nil`
+            guard case .object(let dict) = self else { return nil }
+            // Regular access
             return dict[key]
         }
-        return nil
+        set {
+            var dict = objectValue ?? [:]
+            dict[key] = newValue
+            self = .object(dict)
+        }
     }
 
     /// Dynamic member lookup sugar for string subscripts
     ///
     /// This lets you write `json.foo` instead of `json["foo"]`.
     subscript(dynamicMember member: String) -> JSON? {
-        return self[member]
+        get {
+            return self[member]
+        }
+        set {
+            self[member] = newValue
+        }
     }
     
     /// Return the JSON type at the keypath if this is an `.object`, otherwise `nil`
